6b13ae698b04240d47fa6503fd5fa58f2b125565
herold park <heroldpark@gmail.com>
2024-04-25 22:35:47 +0900

    2024-04-25 dummy

--- backend/src/main/java/shane/blog/controller/EmployeeController.java
+++ backend/src/main/java/shane/blog/controller/EmployeeController.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import shane.blog.entity.Employee;
-import shane.blog.repository.EmployeeRepository;
-
 @RestController
 class EmployeeController {
 
