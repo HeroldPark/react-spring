37edd46df58f1520f828f8b5fd839066fa32e571
heroldpark <heroldpark@gmail.com>
2024-05-07 09:05:09 +0900

    Dummy

--- backend/src/main/java/shane/blog/config/DatabaseConfig.java
+++ backend/src/main/java/shane/blog/config/DatabaseConfig.java
@@ -1,59 +1,68 @@
-// package shane.blog.config;
+package shane.blog.config;
 
-// import com.zaxxer.hikari.HikariConfig;
-// import com.zaxxer.hikari.HikariDataSource;
-// import org.apache.ibatis.session.SqlSessionFactory;
-// import org.mybatis.spring.SqlSessionFactoryBean;
-// import org.mybatis.spring.SqlSessionTemplate;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.context.properties.ConfigurationProperties;
-// import org.springframework.context.ApplicationContext;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.context.annotation.PropertySource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
-// import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-// @Configuration
-// @PropertySource("classpath:/application.properties")
-// public class DatabaseConfig {
+import javax.sql.DataSource;
 
-//     @Autowired
-//     private ApplicationContext context;
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class DatabaseConfig {
 
-//     @Bean
-//     @ConfigurationProperties(prefix = "spring.datasource.hikari")
-//     public HikariConfig hikariConfig() {
-//         return new HikariConfig();
-//     }
+    @Autowired
+    private ApplicationContext context;
 
-//     @Bean
-//     public DataSource dataSource() {
-//         return new HikariDataSource(hikariConfig());
-//     }
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
 
-//     @Bean
-//     public SqlSessionFactory sqlSessionFactory() throws Exception {
-//         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-//         factoryBean.setDataSource(dataSource());
-//         factoryBean.setMapperLocations(context.getResources("classpath:/mybatis/**/*Mapper.xml"));
-//         // factoryBean.setConfiguration(mybatisConfig());
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
 
-//         // 추가: MyBatis 설정 파일을 대체하기 위한 설정
-//         factoryBean.setConfigLocation(context.getResource("classpath:/mybatis/mybatis-config.xml"));
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
 
-//         return factoryBean.getObject();
-//     }
+        // factoryBean.setMapperLocations(context.getResources("classpath:/mybatis/mappers/*Mapper.xml"));
+        // // factoryBean.setConfiguration(mybatisConfig());
 
-//     @Bean
-//     public SqlSessionTemplate sqlSession() throws Exception {
-//         return new SqlSessionTemplate(sqlSessionFactory());
-//     }
+        // 수정된 부분: Mapper XML 파일을 로드하는 방법 변경
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources("classpath:/mybatis/mappers/*Mapper.xml");
+        factoryBean.setMapperLocations(resources);
 
-//     @Bean
-//     @ConfigurationProperties(prefix = "mybatis.configuration")
-//     public org.apache.ibatis.session.Configuration mybatisConfig() {
-//         return new org.apache.ibatis.session.Configuration();
-//     }
+        // 추가: MyBatis 설정 파일을 대체하기 위한 설정
+        // factoryBean.setConfigLocation(context.getResource("classpath:/mybatis/mybatis-config.xml"));
 
-// }
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory());
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration mybatisConfig() {
+        return new org.apache.ibatis.session.Configuration();
+    }
+
+}
